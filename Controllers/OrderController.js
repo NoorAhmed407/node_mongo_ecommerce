e762c7f4418cb8d783a0072478e83608cd85cb14
noorahmed407 <nq73399@gmail.com>
2022-04-25 23:22:31 +0500

    Fetch All orders and update Order status

--- Controllers/OrderController.js
+++ Controllers/OrderController.js
@@ -5,6 +5,8 @@ const authMiddleware = require('./../Middlewares/Authorization');
 
 
 router.post('/order/create', authMiddleware, createOrder );
+router.post('/order/update', authMiddleware, updateOrderStatus );
+router.get('/orders', authMiddleware, getOrderDetails );
 
 async function createOrder(req,res){
    const {user} = req;
@@ -20,5 +22,51 @@ async function createOrder(req,res){
     
 }
 
+async function getOrderDetails(req,res){
+   if(req.user.role !== 1){
+      return res.status(401).json({success: false, message: 'You have no rights to access orders'});
+   }
+   try{
+      const orders = await Orders.find({})
+      .select('-createdAy -updatedAt -__v')
+      .populate({path: 'orderedBy', select: 'name email gender'})
+      .populate(
+         {
+            path: 'orderDetail.product', 
+            select: 'name description categoryId price', 
+            populate: {
+               path: 'categoryId', 
+               select: '-products -createdAt -updatedAt -__v'
+            }
+         }
+         );
+      return res.status(200).json({success: true, message: "Orders Fetched Successfully", data: orders});
+
+   }
+   catch(e){
+      return res.status(500).json({success: false, message: e?.message,});
+
+   }
+}
+
+
+async function updateOrderStatus(req,res){
+   if(req.user.role !== 1){
+      return res.status(401).json({success: false, message: 'You have no rights to update order status'});
+   }
+   try{
+      const order = await Orders.updateOne({_id: req.body.orderId}, {$set: {status: req.body.status}});
+      if(order.modifiedCount){
+         return res.status(200).json({success: true, message: "Order Status Updated Successfully.!"});
+      }
+      else{
+         return res.status(400).json({success: true, message: "Something Went Wrong",});
+      }
+   }
+   catch(e){
+      return res.status(500).json({success: false, message: e?.message,});
+
+   }
+}
 
 module.exports = router;
--- Models/Orders.js
+++ Models/Orders.js
@@ -1,34 +1,37 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const orderDetailSchema = new mongoose.Schema({
-    productId: mongoose.Schema.Types.ObjectId,
-    quantity: Number,
-})
-
-const orderSchema = new mongoose.Schema({
-    orderedBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-    },
-
-    totalPrice: {
-        type: Number,
-        required: true,
-    },
-    status: {
-        type: String,
-        default: 'Pending',
-    },
-    orderDetail: {
-        type: [orderDetailSchema],
-        required: true,
-    }
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Products",
+  },
+  quantity: Number,
 });
 
+const orderSchema = new mongoose.Schema({
+  orderedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+    required: true,
+  },
 
-orderSchema.set('timestamps', true);
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: "Pending",
+  },
+  orderDetail: {
+    type: [orderDetailSchema],
+    required: true,
+  },
+});
 
-const Orders = mongoose.model('Orders', orderSchema);
+orderSchema.set("timestamps", true);
+
+const Orders = mongoose.model("Orders", orderSchema);
 
 module.exports = Orders;
